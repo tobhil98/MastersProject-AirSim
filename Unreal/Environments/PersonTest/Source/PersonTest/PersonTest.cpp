25a41b8cf4a9610bf6e1cbcd9be948431d611ce7
Toby Hillier <44164605+tobhil98@users.noreply.github.com>
2021-04-05 00:45:51 +0100

    Unreal Dev

---
+++ Unreal/Environments/PersonTest/Source/PersonTest/PersonTest.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PersonTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PersonTest, "PersonTest" );
