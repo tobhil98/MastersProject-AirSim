4d9f2dd5054d9b8b0eb53b0b0f1d498e22a32e5f
Toby Hillier <44164605+tobhil98@users.noreply.github.com>
2021-05-09 19:30:15 +0100

    ML training

--- Unity/UnityDemo/Assets/MLTraining/Scripts/MLCar.cs
+++ Unity/UnityDemo/Assets/MLTraining/Scripts/MLCar.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 using AirSimUnity.CarStructs;
 using AirSimUnity;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
+
 
 [RequireComponent(typeof(AirSimCarController))]
 public class MLCar : Agent
@@ -30,23 +32,35 @@ public class MLCar : Agent
     private Vector3 startPos;
     private Quaternion startRotation;
 
-    [SerializeField] private Transform targetTransform;
+    [SerializeField] public Transform targetTransform;
+    [SerializeField] private MLController controller;
 
     private float optimalDistance;
+    private bool moved = false;
+    private int stuckCount = 0;
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        carControls.handbrake = actions.DiscreteActions[0]>0 ? true : false;
+        //carControls.handbrake = actions.DiscreteActions[0]>0 ? true : false;
         carControls.throttle = actions.ContinuousActions[0];
-        carControls.brake = actions.ContinuousActions[1];
-        carControls.steering = actions.ContinuousActions[2];
+        carControls.brake = actions.ContinuousActions[0];
+        carControls.steering = actions.ContinuousActions[1];
     }
+/*    public override void OnActionReceived(float[] vectorAction)
+    {
+        carControls.throttle = vectorAction[0];
+        carControls.brake = vectorAction[0];
+        carControls.steering = vectorAction[1];
+    }*/
 
     public override void CollectObservations(VectorSensor sensor)
     {
         Vector3 v = targetTransform.localPosition - transform.localPosition;
-        float directionDot = Vector3.Dot(transform.forward, v);
+        float directionDot = (float)System.Math.Tanh(Vector3.Angle(transform.forward.normalized, v)/180f);
         sensor.AddObservation(directionDot);
+        float turn = AngleDir(transform.forward, v, transform.up);
+        sensor.AddObservation(turn);
+        //Debug.Log("Observation: " + directionDot + " - " + turn);
     }
 
     public override void OnEpisodeBegin()
@@ -54,23 +68,35 @@ public class MLCar : Agent
         transform.GetComponent<Rigidbody>().velocity = Vector3.zero;
         transform.localPosition = startPos;
         transform.rotation = startRotation;
-        optimalDistance = Vector3.Distance(startPos, targetTransform.localPosition);
 
         transform.localPosition += new Vector3(Random.Range(-5, 5), 0, Random.Range(-5, 5));
         transform.Rotate(0, Random.Range(-30, 100), 0, Space.Self);
         targetTransform.localPosition = new Vector3(Random.Range(-40, 40), targetTransform.localPosition.y, Random.Range(20, 40));
+
+        optimalDistance = Vector3.Distance(transform.localPosition, targetTransform.localPosition);
+
+        SetReward(0);
+        moved = false;
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
+
+
         ActionSegment<float> continousActions = actionsOut.ContinuousActions;
-        ActionSegment<int> descreteActions = actionsOut.DiscreteActions;
+        //ActionSegment<int> descreteActions = actionsOut.DiscreteActions;
         continousActions[0] = Input.GetAxis("Vertical");
-        continousActions[1] = Input.GetAxis("Vertical");
-        continousActions[2] = Input.GetAxis("Horizontal");
-        descreteActions[0] = Input.GetAxis("Jump")>0 ? 1 : 0;
+        //continousActions[1] = Input.GetAxis("Vertical");
+        continousActions[1] = Input.GetAxis("Horizontal");
+        //descreteActions[0] = Input.GetAxis("Jump")>0 ? 1 : 0;
     }
 
+/*    public override void Heuristic(float[] actionsOut)
+    {
+        actionsOut[0] = Input.GetAxis("Vertical");
+        actionsOut[1] = Input.GetAxis("Horizontal");
+    }
+*/
 
     private void Start()
     {
@@ -84,35 +110,93 @@ public class MLCar : Agent
 
     public void FixedUpdate()
     {
-            throttle = carControls.throttle;
-            handBrake = carControls.handbrake ? 1 : 0;
-            footBreak = carControls.brake;
-            steering = carControls.steering;
+        throttle = carControls.throttle;
+        handBrake = 0; //carControls.handbrake ? 1 : 0;
+        footBreak = carControls.brake;
+        steering = carControls.steering;
 
-            carController.Move(steering, throttle, footBreak, handBrake);
-            carController.UpdateCarData(ref carData);
-            carData.throttle = throttle;
-            carData.brake = footBreak;
-            carData.steering = steering;
+        carController.Move(steering, throttle, footBreak, handBrake);
+        carController.UpdateCarData(ref carData);
+        carData.throttle = throttle;
+        carData.brake = footBreak;
+        carData.steering = steering;
 
-        if (Vector3.Distance(transform.localPosition, targetTransform.localPosition) < optimalDistance - 5)
+        /*Debug.LogWarning("HandBreak: " + handBrake);
+        Debug.LogWarning("Steering: " + steering);*/
+        
+        if (Vector3.Distance(transform.localPosition, targetTransform.localPosition) < optimalDistance-1)
         {
+            moved = true;
+            stuckCount = 0;
             AddReward(0.05f);
             optimalDistance = Vector3.Distance(transform.localPosition, targetTransform.localPosition);
+            
+            Vector3 v = Vector3.Scale((targetTransform.position - transform.position), new Vector3(1, 0, 1)).normalized;
+            float a = Vector3.Angle(Vector3.Scale(transform.forward, new Vector3(1, 0, 1)).normalized, v);
+
+            if (a < 10)
+            {
+                if (throttle > 0.2)
+                {
+                    AddReward((10 - a) / 200);
+                }
+            }
+            
+        }
+        else
+        {
+            if (throttle < 0.2)
+            {
+                AddReward(-0.003f);
+            }
         }
 
-        if (Vector3.Distance(transform.localPosition, targetTransform.localPosition) < 15)
+        if (StepCount > 500 && moved == false)
         {
-            SetReward(2f);
+            //PrefabUtility.RevertObjectOverride(gameObject);
+            /*            Destroy(gameObject);
+                        controller.resetMap();*/
+            Debug.Log("StuckCount " + ++stuckCount);
+            EpisodeInterrupted();
+        }
+
+        if (Vector3.Distance(transform.localPosition, targetTransform.localPosition) < 13)
+        {
+            AddReward(10f);
             EndEpisode();
         }
 
-        }
+
+       }
 
     public void ObjectCollided(Collider other)
     {
-        SetReward(-1f);
+        AddReward(-7f);
         EndEpisode();
+    }
+
+    private float AngleDir(Vector3 fwd, Vector3 targetDir, Vector3 up)
+    {
+        Vector3 perp = Vector3.Cross(fwd, targetDir);
+        float dir = Vector3.Dot(perp, up);
+
+        if (dir > 0f)
+        {
+            return 1f;
+        }
+        else if (dir < 0f)
+        {
+            return 0f;
+        }
+        else
+        {
+            return 0.5f;
+        }
+    }
+
+    private void OnDisable()
+    {
+        Debug.LogError("Car dissabled");
     }
 
 }
---
+++ Unity/UnityDemo/Assets/MLTraining/Scripts/MLController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MLController : MonoBehaviour
+{
+    public Transform car;
+    public Transform target;
+    public Transform carPrefab;
+
+    private Vector3 carInitPos;
+
+    private Transform parent;
+
+    public void Start()
+    {
+        carInitPos = car.position;
+
+        parent = car.parent;
+    }
+
+
+    public void resetMap()
+    {
+        Transform t = Instantiate(carPrefab);
+        t.parent = parent;
+        t.position = carInitPos;
+        t.GetComponent<MLCar>().targetTransform = target;
+        Debug.LogError("MapReset");
+    }
+
+}
